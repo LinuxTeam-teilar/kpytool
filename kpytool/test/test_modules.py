d07a247407ca3ad97fbb9dac913614f5bd68220b
Giorgos Tsiapaliokas <terietor@gmail.com>
2012-08-22 00:51:39 +0300

    Do some basic checks

--- kpytool/test/test_modules.py
+++ kpytool/test/test_modules.py
@@ -18,6 +18,8 @@
 """
 modules.py does the above,
 * download the tarball 'kpytool-configs' and the file '.kpytool.cfg'
+This is automatically checked inside the kpytool, if the above doesn't
+exist an exception will be raised
 * check if the below dirs exist and if they valid according to the ones from the .kpytool.cfg
     **kpytool-configs
     **kde-source
@@ -29,4 +31,49 @@ from the kpytool.cfg file
 *check if ModuleReader.moduleInfo property returns valid data
 """
 
-import modules
+from kpytool.modules import ModuleReader
+from os import path, environ
+import logging
+
+logger = logging.getLogger('test_kpytool.test_modules')
+
+MODULE_NAME = 'kactivities'
+
+def do():
+
+    #initialize
+    p = path.join(environ['HOME'], '.kpytool.cfg')
+    logger.debug(p)
+    reader = ModuleReader(MODULE_NAME, p)
+
+    reader.moduleName = MODULE_NAME
+
+    #check module info
+    logger.debug('module name' + reader.moduleInfo[0])
+    logger.debug('parent path' + reader.moduleInfo[1])
+
+    _checkPath('source path', reader.moduleInfo[2])
+
+    for vcs, link, branch in reader.moduleInfo[3]:
+        logger.debug('vcs:' + vcs)
+        logger.debug('link:' + link)
+        logger.debug('branch:' + branch)
+
+    _checkPath('build path', reader.moduleInfo[4])
+    _checkPath('install path', reader.moduleInfo[5])
+
+    logger.debug('build system args' + reader.moduleInfo[6])
+
+    #check default modules
+    l = [
+        'automoc',
+        'cagibi',
+        'akonadi'
+    ]
+    if len(returnsMatches(reader.moduleList, l)) != 3:
+        logger.debug('the default list modules aren\'t correct')
+
+def _checkPath(pathName, path):
+    if not path.exists(path) && is not path.isdir(path):
+        logger.debug(path + 'doesn\'t exist!!!!!')
+    else logger.debug(pathName + path)
