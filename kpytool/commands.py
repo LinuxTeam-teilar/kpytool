ca98642826b91382c8057bd1563423a3247e01d0
Giorgos Tsiapaliokas <terietor@gmail.com>
2012-09-26 18:31:45 +0300

    Add notes and stuff like that for the commands.py

--- kpytool/commands.py
+++ kpytool/commands.py
@@ -14,3 +14,40 @@
 #  along with this program; see the file COPYING.  If not, write to
 #  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301, USA.
+
+import logging
+from os import path
+
+logger = logging.getLogger('kpytool.commands')
+
+class Commands(object):
+    def __init__(self, moduleName, moduleParent):
+        #TODO fix this brain dead line!!!
+        print "Preparing " + moduleName + "from " + moduleParent
+
+
+    def src(self, source_path):
+        #check if we have a svn or git or bzr repo
+        if path.exists(path.join(source_path + '.git')):
+            #Do some git stuff
+        elif path.exists(path.join(source_path + '.svn')):
+            #Do some svn stuff
+        elif path.exists(path.join(source_path + '.bzr')):
+            #Do some bzr stuff
+
+    def configure(self, source_path, build_path):
+        if path.exists(path.join(source_path + 'CMakeLists.txt')):
+            #Do some cmake stuff
+        elif if path.exists(path.join(source_path + '*.pro')):
+            #Do some autotools stuff
+        elif if path.exists(path.join(source_path + 'makefile.am')):
+            #Do bla bla
+
+    def build(self, build_path):
+        #build the project
+
+    def install(self, build_path):
+        #install the project
+
+
+
